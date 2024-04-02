72b275961d11aff6d9ef783330db02392f4f1dd6
Jens Nie <jneines@web.de>
2024-04-02 18:25:29 +0200

    Removed unsuitable tests as well as dependencies in pyproject.toml

--- tests/test_async_app.py
+++ tests/test_async_app.py
@@ -4,30 +4,4 @@
 
 
 import unittest
-from click.testing import CliRunner
 
-from async_app import async_app
-from async_app import cli
-
-
-class TestAsync_app(unittest.TestCase):
-    """Tests for `async_app` package."""
-
-    def setUp(self):
-        """Set up test fixtures, if any."""
-
-    def tearDown(self):
-        """Tear down test fixtures, if any."""
-
-    def test_000_something(self):
-        """Test something."""
-
-    def test_command_line_interface(self):
-        """Test the CLI."""
-        runner = CliRunner()
-        result = runner.invoke(cli.main)
-        assert result.exit_code == 0
-        assert "async_app.cli.main" in result.output
-        help_result = runner.invoke(cli.main, ["--help"])
-        assert help_result.exit_code == 0
-        assert "--help  Show this message and exit." in help_result.output
