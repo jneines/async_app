c2c55beef39464ee04da21c6b2a31ec9b3495a2f
Jens Nie <jneines@web.de>
2024-04-02 18:37:41 +0200

    Added two simple tests to make the corresponding actions happy.

--- tests/test_async_app.py
+++ tests/test_async_app.py
@@ -5,3 +5,35 @@
 
 import unittest
 
+from async_app.app import AsyncApp
+
+class TestAddTaskDescriptions(unittest.TestCase):
+
+    def test_add_one_continuous_task(self):
+        task_descriptions = [
+                {
+                    "kind": "continuous",
+                    "function": lambda :print("Hello"),
+                    }
+                ]
+        app = AsyncApp()
+        for task_description in task_descriptions:
+            app.add_task_description(task_description)
+
+        self.assertEqual(len(app.task_descriptions["continuous"]), 1)
+
+    def test_add_one_periodic_task(self):
+        task_descriptions = [
+                {
+                    "kind": "periodic",
+                    "function": lambda :print("Hello"),
+                    "frequency": 1,
+                    }
+                ]
+        app = AsyncApp()
+        for task_description in task_descriptions:
+            app.add_task_description(task_description)
+
+        self.assertEqual(len(app.task_descriptions["periodic"]), 1)
+
+
